56bf10dcf37bf0d4b172a0236d071fc8a40969d3
Rehan Manzoor <rehanmanzoor@developer.pc>
2021-04-09 01:11:55 +0500

    Fix title height

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -32,7 +32,7 @@
                             class="card-img-top"></div>
                         <div class="card-body">
                             <h5 class="card-title">$ {{ $item->price }}</h5>
-                            <p class="card-text">{{ \Illuminate\Support\Str::limit($item->title, 20) }}</p>
+                            <p class="card-text" style="min-height:2rem;">{{ \Illuminate\Support\Str::limit($item->title, 30) }}</p>
                             <a href="{{ route('edit-item', ['id' => $item->id]) }}" class="btn btn-primary">Edit</a>
                         </div>
                     </div>
